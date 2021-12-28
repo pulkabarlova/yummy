c3949c7b9671255239250925821648992aea5806
polina <barlovapolina@gmail.com>
2021-12-28 18:47:00 +0300

    Add file

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import pygame
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+width1 = 40
+height1 = 300
+x = 136.5
+y = 0
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+screen.fill(pygame.Color("black"))
+pygame.display.set_caption("Нажми на экран")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            coord = event.pos
+            pygame.draw.ellipse(screen, pygame.Color("yellow"),
+                                (coord[0], coord[1], random.choice(range(1, 100)), random.choice(range(1, 100))))
+    pygame.display.flip()
+pygame.quit()
